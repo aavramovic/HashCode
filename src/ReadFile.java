91d9e67212ea60346d1221b0281581cbf30b3e9c
Антонио Аврамовиќ <Antonio.avramovic@gmail.com>
2020-02-21 08:01:39 +0100

    wip

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
-import java.io.*;
 
 class Library {
     int id;
@@ -28,102 +31,28 @@ class Library {
     }
 
     public Integer Grade() {
+        //Grade libraries
+        //Grade books
         int grade = booksPerLibrary.size() / booksPerDay + totalScore / signUpTime;
         return 0;
     }
 
 }
 
+class Solution{
+    List<Integer> listLibraries = new ArrayList<>();
+    List<Integer> listAvailableBooks = new ArrayList<>();
+    List<List<Integer>> listList = new ArrayList<>();
+    String fileOut;
 
-public class ReadFile {
-    public static void readFileMethod(String fileIn) throws IOException {
-        List<String> lines = new LinkedList<>();
-        Scanner scanner = new Scanner(new File("src/Input/" + fileIn));
-        int numOfBooks = Integer.parseInt(scanner.next());
-        int numOfLibraries = Integer.parseInt(scanner.next());
-        int daysForScanning = Integer.parseInt(scanner.next());
-        List<Integer> bookScores = new ArrayList<>();
-        for (int i = 0; i < numOfBooks; i++) {
-            bookScores.add(Integer.parseInt(scanner.next()));
-        }
-        Library.bookScores = bookScores;
-        List<Library> libraries = new ArrayList<>();
-        HashSet<Integer> booksScanned = new HashSet<>();
-
-        List<Integer> booksInLibrary;
-        for (int i = 0; i < numOfLibraries; i++) {
-            booksInLibrary = new ArrayList<>();
-            int numberOfBooks = Integer.parseInt(scanner.next());
-            int signUpTime = Integer.parseInt(scanner.next());
-            int booksPerDay = Integer.parseInt(scanner.next());
-            for (int j = 0; j < numberOfBooks; j++) {
-                int bookId = Integer.parseInt(scanner.next());
-                booksInLibrary.add(bookId);
-            }
-            libraries.add(new Library(i, booksInLibrary, booksPerDay, signUpTime));
-        }
-        libraries.sort(Comparator.comparing(Library::Grade));
-        int numberOfLibraries = 0;
-        int daysPassed = 0;
-        for (Library library : libraries) {
-            if (daysPassed < daysForScanning) {
-                numberOfLibraries++;
-                daysPassed += library.signUpTime;
-                library.startingDay = daysPassed;
-            }
-        }
-        if (daysPassed > daysForScanning)
-            numberOfLibraries--;
-
-        FileWriter fileWriter = new FileWriter("src/Output/" + fileIn);
-
-//        System.out.println(numberOfLibraries);
-        List<Integer> listLibraries = new ArrayList<>();
-        List<Integer> listAvailableBooks = new ArrayList<>();
-        List<List<Integer>> listList = new ArrayList<>();
-        for (int i = 0; i < numberOfLibraries; i++) {
-            Library library = libraries.get(i);
-            int availableBooks = 0;
-
-            List<Integer> books = new ArrayList<>();
-//            System.out.println(library.id);
-            int realisticNumberOfBooks =
-                    Math.min(library.numberOfBooks, (daysForScanning - library.startingDay + 1) * library.booksPerDay);
-
-//            System.out.println(realisticNumberOfBooks);
-            for (int j = 0; j < library.numberOfBooks; j++) {
-                int book = library.booksPerLibrary.get(j);
-                if (bookScores.get(book) >= library.averageScore && !booksScanned.contains(book) && availableBooks < realisticNumberOfBooks) {
-                    booksScanned.add(book);
-                    availableBooks++;
-                    books.add(book);
-//                    fileWriter.write(book + " ");
-//                    System.out.println(book);
-                }
-            }
-            for (int j = 0; j < library.numberOfBooks; j++) {
-                int book = library.booksPerLibrary.get(j);
-                if (bookScores.get(book) < library.averageScore && !booksScanned.contains(book) && availableBooks < realisticNumberOfBooks) {
-                    booksScanned.add(book);
-                    availableBooks++;
-                    books.add(book);
-//                    fileWriter.write(book + " ");
-//                    System.out.println(book);
-                }
-            }
-            if (availableBooks > 0) {
-                listLibraries.add(library.id);
-                listAvailableBooks.add(availableBooks);
-                List<Integer> temp = new ArrayList<>();
-
-                for (int book : books) {
-                    temp.add(book);
-                }
-                listList.add(temp);
-            }
-
-
-        }
+    public Solution(List<Integer> listLibraries, List<Integer> listAvailableBooks, List<List<Integer>> listList, String fileOut) {
+        this.listLibraries = listLibraries;
+        this.listAvailableBooks = listAvailableBooks;
+        this.listList = listList;
+        this.fileOut = fileOut;
+    }
+    public void WriteList() throws IOException {
+        FileWriter fileWriter = new FileWriter("src/Output/" + fileOut);
         fileWriter.write(listLibraries.size() + "\n");
         for (int i = 0; i < listLibraries.size(); i++) {
             fileWriter.write(listLibraries.get(i) + " ");
@@ -136,16 +65,96 @@ public class ReadFile {
         }
         fileWriter.close();
     }
+}
+class Problem{
+    int numOfBooks;
+    int numOfLibraries;
+    int daysForScanning;
+    String fileIn;
+    List<Integer> bookScores = new ArrayList<>();
+    List<Library> libraries = new ArrayList<>();
+    Set<Integer> booksScanned = new HashSet<>();
+    List<Integer> booksInLibrary = new ArrayList<>();
+    Solution solution;
 
-//    public void WriteList(List<Integer> list, String fileOut) throws IOException {
-//        FileWriter fileWriter = new FileWriter(fileOut);
-//        fileWriter.write(list.size() + "\n");
-//        for (Integer item : list) {
-//            fileWriter.write(item + " ");
-//        }
-//        fileWriter.close();
-//    }
+    List<Integer> listLibraries = new ArrayList<>();
+    List<Integer> listAvailableBooks = new ArrayList<>();
+    List<List<Integer>> listList = new ArrayList<>();
+    public Problem(String fileIn) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/Input/" + fileIn));
+        numOfBooks = Integer.parseInt(scanner.next());
+        numOfLibraries = Integer.parseInt(scanner.next());
+        daysForScanning = Integer.parseInt(scanner.next());
+        bookScores = new ArrayList<>();
+        for (int i = 0; i < numOfBooks; i++) {
+            bookScores.add(Integer.parseInt(scanner.next()));
+        }
+        Library.bookScores = bookScores; //static
+        for (int i = 0; i < numOfLibraries; i++) {
+            booksInLibrary = new ArrayList<>();
+            int numberOfBooks = Integer.parseInt(scanner.next());
+            int signUpTime = Integer.parseInt(scanner.next());
+            int booksPerDay = Integer.parseInt(scanner.next());
+            for (int j = 0; j < numberOfBooks; j++) {
+                int bookId = Integer.parseInt(scanner.next());
+                booksInLibrary.add(bookId);
+            }
+            libraries.add(new Library(i, booksInLibrary, booksPerDay, signUpTime));
+        }
 
+    }
+
+    public void Solve() throws IOException {
+        libraries.sort(Comparator.comparing(Library::Grade));
+        int numberOfLibraries = 0;
+        int daysPassed = 0;
+        for (Library library : libraries) {
+            if (daysPassed < daysForScanning) {
+                numberOfLibraries++;
+                daysPassed += library.signUpTime;
+                library.startingDay = daysPassed;
+            }
+        }
+        if (daysPassed > daysForScanning)
+            numberOfLibraries--;
+        for (int i = 0; i < numberOfLibraries; i++) {
+            Library library = libraries.get(i);
+            int availableBooks = 0;
+
+            List<Integer> books = new ArrayList<>();
+            int realisticNumberOfBooks =
+                    Math.min(library.numberOfBooks, (daysForScanning - library.startingDay + 1) * library.booksPerDay);
+
+            for (int j = 0; j < library.numberOfBooks; j++) {
+                int book = library.booksPerLibrary.get(j);
+                if (bookScores.get(book) >= library.averageScore && !booksScanned.contains(book) && availableBooks < realisticNumberOfBooks) {
+                    booksScanned.add(book);
+                    availableBooks++;
+                    books.add(book);
+                }
+            }
+            for (int j = 0; j < library.numberOfBooks; j++) {
+                int book = library.booksPerLibrary.get(j);
+                if (bookScores.get(book) < library.averageScore && !booksScanned.contains(book) && availableBooks < realisticNumberOfBooks) {
+                    booksScanned.add(book);
+                    availableBooks++;
+                    books.add(book);
+                }
+            }
+            if (availableBooks > 0) {
+                listLibraries.add(library.id);
+                listAvailableBooks.add(availableBooks);
+
+                List<Integer> temp = new ArrayList<>(books);
+                listList.add(temp);
+            }
+            solution = new Solution(listLibraries, listAvailableBooks, listList, fileIn);
+        }
+
+    }
+}
+
+public class ReadFile {
     public static void main(String[] args) throws IOException {
         String[] listIn = new String[6];
         listIn[0] = ("a_example.txt");
@@ -154,16 +163,10 @@ public class ReadFile {
         listIn[3] = ("d_tough_choices.txt");
         listIn[4] = ("e_so_many_books.txt");
         listIn[5] = ("f_libraries_of_the_world.txt");
-
-        //        String[] listOut = new String[6];
-//        listOut[0] = ("src/a_solution.out");
-//        listOut[1] = ("src/b_solution.out");
-//        listOut[2] = ("src/c_solution.out");
-//        listOut[3] = ("src/d_solution.out");
-//        listOut[4] = ("src/e_solution.out");
-//        ReadFile ex = new ReadFile();
         for (int i = 0; i < 6; i++) {
-            readFileMethod(listIn[i]);
+            Problem problem = new Problem(listIn[i]);
+            problem.Solve();
+            problem.solution.WriteList();
         }
     }
 }
